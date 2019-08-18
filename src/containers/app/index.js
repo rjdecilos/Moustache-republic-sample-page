094764c831b70ac96512b73bb9105acd0a3b4293
rjdecilos <decilos_rj@yahoo.com>
2019-08-18 02:00:29 -0500

    fixed margin

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
 
-const App = () => (
-  <div>
+const App = () => {
+
+  useEffect(() => {
+    document.body.setAttribute('style', 'margin: 0')
+  });
+  return (<div>
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
     </main>
-  </div>
-)
+  </div>)
+}
 
 export default App
